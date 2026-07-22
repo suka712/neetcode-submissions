8edd5e696290dad19f6acd7e5fba10179cbef966
Khiem Nguyen <khiem@sukaseven.com>
2026-07-22 15:17:12 +0800

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/dynamicArray/submission-20.py
@@ -0,0 +1,40 @@
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.arr = [0] * capacity
+        self.size = 0
+        self.capacity = capacity
+
+    def get(self, i: int) -> int:
+        return self.arr[i]
+
+
+    def set(self, i: int, n: int) -> None:
+        self.arr[i] = n
+
+
+    def pushback(self, n: int) -> None:
+        if (self.size == self.capacity):
+            self.resize()
+            
+        self.arr[self.size] = n
+        self.size += 1
+
+
+    def popback(self) -> int:
+        val = self.arr[self.size - 1]
+        self.size -= 1
+        return val
+
+
+    def resize(self) -> None:
+        self.arr = self.arr + ([0] * self.capacity)
+        self.capacity *= 2
+
+
+    def getSize(self) -> int:
+        return self.size
+        
+    
+    def getCapacity(self) -> int:
+        return self.capacity
