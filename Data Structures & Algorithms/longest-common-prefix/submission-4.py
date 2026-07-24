5580ebaf55d4e71cc4981d3279bd3d25a09d1fdc
Khiem Nguyen <khiem@sukaseven.com>
2026-07-24 13:27:59 +0800

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = Node()
+            curr = curr.children[char]
+        curr.is_word = True
+
+    # def startsWith(self, prefix: str) -> bool:
+    #     curr - self.root
+    #     for char in prefix:
+    #         if char not in curr.children:
+    #             return False
+    #         curr = curr.children[char]
+    #     return True
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        t = Trie()
+
+        for i in range(len(strs)):
+            t.insert(strs[i])
+
+        longest = ""
+        curr = t.root
+
+        while len(curr.children) == 1 and not curr.is_word:
+            char = list(curr.children.keys())[0]
+            longest += char
+            curr = curr.children[char]
+
+        return longest
+
+        
+        
